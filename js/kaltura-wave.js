9ba6c56767f2fbc62a308cb4ddc60c3018aa3718
papyromancer <papyromancer@papyromancer.com>
2009-09-19 11:15:25 -0500

    properly settin up tabs in init

--- js/kaltura-wave.js
+++ js/kaltura-wave.js
@@ -1,9 +1,20 @@
-tabs.addTab("Play", {
-                    contentContainer: document.getElementById("kplayer")
-                    });
-tabs.addTab("Edit", {
-                    contentContainer: document.getElementById("keditor")
-                    });
-tabs.addTab("Add Media", {
-                         contentContainer: document.getElementById("kmedia_adder")
-                         });
+// Set Player as default tab
+var tabs = new gadgets.TabSet(__MODULE_ID__, "Play");
+
+// Initialization
+function init() {
+
+  // Define Tabs
+  tabs.addTab("Play", {
+                      contentContainer: document.getElementById("kplayer")
+                      });
+  tabs.addTab("Edit", {
+                      contentContainer: document.getElementById("keditor")
+                      });
+  tabs.addTab("Add Media", {
+                           contentContainer: document.getElementById("kmedia_adder")
+                           });
+}
+
+// Call init function to initialize and display tabs.
+gadgets.util.registerOnLoadHandler(init);
