3bd5bd52d7075f66613bc8d12a49eb5b6adb6540
papyromancer <papyromancer@papyromancer.com>
2009-09-19 11:08:54 -0500

    changing how tabs are called, height to 370

--- js/kaltura-wave.js
+++ js/kaltura-wave.js
@@ -1,3 +1,9 @@
-tabs.addTab("Play", "kplayer");
-tabs.addTab("Edit", "keditor");
-tabs.addTab("Add Media", "kmedia_adder");
+tabs.addTab("Play", {
+                    contentContainer: document.getElementById("kplayer")
+                    });
+tabs.addTab("Edit", {
+                    contentContainer: document.getElementById("keditor")
+                    });
+tabs.addTab("Add Media", {
+                         contentContainer: document.getElementById("kmedia_adder")
+                         });
